d98aca2b1c0776e7228f9b009fdd15c41d662f59
Ka Hei (Jessie) Kong <KKong76@students.claremontmckenna.edu>
2026-04-07 17:00:17 -0700

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -9,7 +9,7 @@ for problem in sql/*; do
     problem_id=$(basename ${problem%.sql})
     result="results/$problem_id.out"
     expected="expected/$problem_id.out"
-    psql postgresql://postgres:pass@localhost:5435/postgres < $problem > $result
+    psql -h localhost -p 5435 -U postgres -d postgres < $problem > $result
     DIFF=$(diff -B $expected $result)
     if [ -z "$DIFF" ]; then
         echo pass
