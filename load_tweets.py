537951cc7d0d76fd4e208f3b8ee5bf09b1b6c4c3
Ka Hei (Jessie) Kong <KKong76@students.claremontmckenna.edu>
2026-04-07 17:37:37 -0700

    Update load_tweets.py

--- load_tweets.py
+++ load_tweets.py
@@ -277,7 +277,7 @@ def insert_tweet(connection,tweet):
             'id_tweets':tweet['id'],
             'id_users':tweet['user']['id'],
             'created_at':remove_nulls(tweet['created_at']),
-	        'in_reply_to_status_id':tweet['in_reply_to_status_id'],
+			'in_reply_to_status_id':tweet['in_reply_to_status_id'],
             'in_reply_to_user_id':tweet['in_reply_to_user_id'],
             'quoted_status_id':tweet.get('quoted_status_id',None),
             'retweet_count':tweet['retweet_count'],
